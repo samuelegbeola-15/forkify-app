5d01b678b1e1f6ca367028cce660f62e38fec5ae
samuelegbeola-15 <egbeolasamuel@gmail.com>
2024-04-19 15:30:43 +0100

    version 1.1

---
+++ src/js/helpers.js
@@ -0,0 +1,45 @@
+import * as config from './config.js';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} seconds`));
+    }, s * 1000);
+  });
+};
+
+export const getJSON = async function (url) {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    // this catches and displays the error
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const sendJSON = async function (url, uploadData) {
+  try {
+    const fetchPro = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(uploadData),
+    });
+
+    // const res = await Promise.race([(fetchPro, timeout(config.TIMEOUT_SEC))]);
+    // const data = await res.json();
+    const data = await fetchPro.json();
+
+    // this catches and displays the error
+    // if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    if (!fetchPro.ok) throw new Error(`${data.message} (${fetchPro.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
---
+++ src/js/model.js
@@ -0,0 +1,159 @@
+// import { set } from 'core-js/core/dict';
+import * as config from './config.js';
+import * as helper from './helpers.js';
+
+export const state = {
+  recipe: {},
+  search: {
+    query: '',
+    results: [],
+    page: 1,
+    resultsPerPage: config.RES_PER_PAGE,
+  },
+  bookmarks: [],
+};
+
+const createRecipeObject = function (data) {
+  let { recipe } = data.data;
+  return {
+    id: recipe.id,
+    title: recipe.title,
+    publisher: recipe.publisher,
+    sourceUrl: recipe.sourceUrl,
+    image: recipe.image_url,
+    servings: recipe.servings,
+    cookingTime: recipe.cooking_time,
+    ingredients: recipe.ingredients,
+    ...(recipe.key && { key: recipe.key }), // this returns the fiest FALSE
+  };
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await helper.getJSON(
+      `${config.API_URL}/${id}?key=${config.KEY}`
+    );
+    state.recipe = createRecipeObject(data);
+
+    if (state.bookmarks.some(bookmark => bookmark.id === id)) {
+      state.recipe.bookmarked = true;
+    } else state.recipe.bookmarked = false;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await helper.getJSON(
+      `${config.API_URL}?search=${query}&key=${config.KEY}`
+    );
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+        ...(rec.key && { key: rec.key }), // this returns the fiest FALSE
+      };
+    });
+
+    state.search.page = 1;
+  } catch (err) {
+    console.error(`${err} 🔥🔥`);
+    throw err;
+  }
+};
+loadSearchResults('pizza');
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = state.search.resultsPerPage * (page - 1);
+  const end = state.search.resultsPerPage * page;
+  return state.search.results.slice(start, end);
+};
+
+export const updateServings = function (newServings) {
+  state.recipe.ingredients.forEach(ing => {
+    ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+  });
+
+  state.recipe.servings = newServings;
+};
+
+const persistBookmarks = function () {
+  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+};
+
+export const addBookmark = function (recipe) {
+  // Add bookmark
+  state.bookmarks.push(recipe);
+
+  // Mark current recipe as bookmarked
+  if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+
+  // Save bookmark data to local storage
+  persistBookmarks();
+};
+
+export const removeBookmark = function (id) {
+  // Remove bookmark
+  const index = state.bookmarks.findIndex(el => el.id === id);
+  state.bookmarks.splice(index, 1);
+
+  // UNmark current recipe as bookmarked
+  if (id === state.recipe.id) state.recipe.bookmarked = false;
+
+  // Save bookmark data to local storage
+  persistBookmarks();
+};
+
+const init = function () {
+  const storage = localStorage.getItem('bookmarks');
+  if (storage) state.bookmarks = JSON.parse(storage);
+};
+init();
+
+const clearBookmarks = function () {
+  localStorage.clear('bookmarks');
+};
+// clearBookmarks();
+
+export const uploadRecipe = async function (newRecipe) {
+  try {
+    const ingredients = Object.entries(newRecipe)
+      .filter(entry => entry[0].startsWith('ingredient') && entry[1] !== '')
+      .map(ing => {
+        const ingArr = ing[1].replaceAll(' ', '').split(',');
+        if (ingArr.length !== 3)
+          throw new Error(
+            `Wrong ingredient format! Please use the correct format`
+          );
+
+        const [quantity, unit, description] = ingArr;
+        return { quantity: quantity ? +quantity : null, unit, description };
+      });
+
+    const recipe = {
+      title: newRecipe.title,
+      source_url: newRecipe.sourceUrl,
+      image_url: newRecipe.image,
+      publisher: newRecipe.publisher,
+      cooking_time: +newRecipe.cookingTime,
+      servings: +newRecipe.servings,
+      ingredients,
+    };
+
+    const data = await helper.sendJSON(
+      `${config.API_URL}?key=${config.KEY}`,
+      recipe
+    );
+    state.recipe = createRecipeObject(data);
+    addBookmark(state.recipe);
+  } catch (err) {
+    throw err;
+  }
+};
